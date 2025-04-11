c6d0d0a1535d93ffa82351a7835f5ee1b377ecd3
Aymeric <aymeric.simao@gmail.com>
2025-04-11 11:54:38 +0200

    MaJ avec year au lieu de date

--- script.js
+++ script.js
@@ -3,5 +3,5 @@
 fetch('https://whatyearisit-backend-iota-dun.vercel.app/year')
 .then(response => response.json())
 .then(data => {
-    document.querySelector('#year').textContent = data.date
+    document.querySelector('#year').textContent = data.year
 })
